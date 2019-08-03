eaabeeabf0425e3f27db3595f2b0054b2734ab5f
cb0806151 <cb0806151@otc.edu>
2019-08-03 14:07:36 -0500

    creating working branch

--- public/js/app.js
+++ public/js/app.js
@@ -57837,8 +57837,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\dashboard\laravel\College\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\dashboard\laravel\College\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Raxsus\Desktop\College\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Raxsus\Desktop\College\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
