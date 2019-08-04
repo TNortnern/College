5260d3f291a77f657968ffc7713031553264d1b6
cb0806151 <cb0806151@otc.edu>
2019-08-03 20:55:45 -0500

    Integrate Login Modal

--- resources/js/components/LoginModal.vue
+++ resources/js/components/LoginModal.vue
@@ -3,11 +3,18 @@
         <div class="modal-container">
             <div class="modal-sidebar-container">
                 <div class="modal-close-button" @click='closeModal()'>X</div>
-                <div class="modal-sidebar">
-                    <input type="text" class="modal-input" placeholder="email">
-                    <input type="password" class="modal-input" placeholder="password">
-                    <div class="button">Login</div>
-                </div>
+                <!-- <div class="modal-sidebar"> -->
+                    <form @submit.prevent="login" method="POST" class="modal-sidebar">
+                        <div class="alert alert-danger" id="invalid-login">Invalid Login Credentials!</div>
+                        <input v-model="email" type="text" class="modal-input" placeholder="email">
+                        <input v-model="password" type="password" class="modal-input" placeholder="password">
+                        <div>
+                            <label for="remember">Remember Me</label>
+                            <input v-model="remember" name="remember" type="checkbox" />
+                        </div>
+                        <button id="loginbutton" class="button">Login</button>
+                    </form>
+                <!-- </rdiv> -->
             </div>
             <div class="modal-description">
                 <p>Campus<br>Login</p>
@@ -18,13 +25,70 @@
 
  <script>
 import './../../sass/login-modal.scss'
+import $ from "jquery";
+window.axios = require("axios");
+// For adding the token to axios header (add this only one time).
+window.axios.defaults.headers.common = {
+  _token: document
+    .querySelector('meta[name="csrf-token"]')
+    .getAttribute("content"),
+  "X-Requested-With": "XMLHttpRequest",
+  "X-CSRF-TOKEN": document
+    .querySelector('meta[name="csrf-token"]')
+    .getAttribute("content")
+};
 export default {
-   name: 'login-modal',
-  methods: {
-      closeModal() {
-          this.$emit("closeModal", true)
-      }
+  name: 'login-modal',
+  data() {
+        return {
+        email: "",
+        password: "",
+        remember: "",
+        errors: ""
+        };
   },
-  created() {}
+  methods: {
+        closeModal() {
+            this.$emit("closeModal", true)
+        },
+        login() {
+        this.errors = "";
+        $("#invalid-login").hide();
+        $("#page-loader").show();
+        $("#loginbutton").attr("disabled", true);
+        axios
+            .post("/signin", {
+            email: this.email,
+            password: this.password
+            })
+            .then(res => {
+            $("#page-loader").show();
+            if (res.data != false) {
+                window.location.href = "/";
+            } else {
+                $("#loginbutton").attr("disabled", false);
+                $("#invalid-login").show();
+                $("#page-loader").hide();
+            }
+            })
+            .catch(err => {
+            $("#loginbutton").attr("disabled", false);
+            $("#page-loader").hide();
+
+            this.errors = err.response.data.errors;
+            console.log(err);
+            });
+        }
+  },
+  created() {},
+  mounted() {},
+
+  computed: {
+    validationErrors() {
+      let errors = Object.values(this.errors);
+      errors = errors.flat();
+      return errors;
+    }
+  }
 };
 </script> 
