e42a1eaf513397de53fad9f0a0ff786f2a40dac7
cb0806151 <cb0806151@otc.edu>
2019-08-06 18:28:52 -0500

    implement carousel and footer on home page

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,18 +1,8 @@
 <template>
     <div>
         <navbar></navbar>
+        <carousel :carouselData='carouselData'></carousel>
         <div class="landing-page-shell">
-            <div class="title-and-search-modal">
-                <p class="landing-page-title">Super Cool College of Information Technologies</p>
-                <input type="text" placeholder="What are you looking for?" class="landing-page-search-bar">
-                <div class="landing-page-navigation">
-                    <p>Future Students</p>
-                    <p>Programs and Courses</p>
-                    <p>Admissions</p>
-                    <p>Current Students</p>
-                    <p>More Students</p>
-                </div>
-            </div>
             <div class="left-info-block-and-image-container">
                 <div class="info-block">
                     <table>
@@ -200,6 +190,7 @@
                     </div>
                 </div>
             </div>
+            <globalFooter></globalFooter>
         </div>
     </div>
 </template>
@@ -207,6 +198,8 @@
 <script>
     import './../../sass/home.scss'
     import navbar from './Navbar'
+    import carousel from './Carousel'
+    import globalFooter from './Footer'
     
     window.axios = require("axios");
     // For adding the token to axios header (add this only one time).
@@ -223,6 +216,8 @@
         export default {
         components: {
             navbar,
+            carousel,
+            globalFooter
         },
         data: function() {
             return {
@@ -234,7 +229,19 @@
                 password: "",
                 confirm: "",
                 pnum: "",
-                errors: ""
+                errors: "",
+                carouselData: {
+                    carouselImage: [
+                        'https://images.unsplash.com/photo-1523050854058-8df90110c9f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2626&q=80',
+                        'https://images.unsplash.com/photo-1523240795612-9a054b0db644?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3900&q=80',
+                        'https://images.unsplash.com/photo-1549163986-da279e1f27c7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2448&q=80'
+                    ],
+                    carouselText: [
+                        'Our University supports the highest graduation percentage in the country at 75%.',
+                        'A wealth of tutoring services supply students with everything they need to succeed.',
+                        'Set in the heart of Ontarios plains, our campus is over 100 years old (and I daresay aging gracefully)'
+                    ]
+                }
             }
         },
         methods: {
