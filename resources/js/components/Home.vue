18a83e1e898adbef4ce113cb668ab3fcfb4957a3
cb0806151 <cb0806151@otc.edu>
2019-08-08 20:24:27 -0500

    remove commented out code

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -3,17 +3,6 @@
         <navbar></navbar>
         <carousel :carouselData='carouselData'></carousel>
         <div data-aos-offset="0" data-aos="fade-in" class="landing-page-shell">
-            <!-- <div class="title-and-search-modal">
-                <p class="landing-page-title">Super Cool College of Information Technologies</p>
-                <input type="text" placeholder="What are you looking for?" class="landing-page-search-bar">
-                <div class="landing-page-navigation">
-                    <p>Future Students</p>
-                    <p>Programs and Courses</p>
-                    <p>Admissions</p>
-                    <p>Current Students</p>
-                    <p>More Students</p>
-                </div>
-            </div> -->
             <div data-aos="fade-in-right" class="left-info-block-and-image-container">
                 <div class="info-block">
                     <table>
@@ -292,10 +281,7 @@
             }
         },
         created(){
-            console.log("created")
-            AOS.init({
-                once: true,
-            });
+            AOS.refreshHard();
         },
         mounted() {
            
