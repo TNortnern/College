2c02784552ac7e0a9dd66c5a6b00e1c04cd43842
Tray Northern <tnorthern@yahoo.com>
2019-08-10 14:18:03 -0500

    title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="login-status" content="{{ Auth::check() }}">
-        <title>Laravel</title>
+        <title>UOGO</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
